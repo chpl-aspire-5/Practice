3810b150b0db27ce731dd2ab349fac301658bc36
Aziz Kachwala <aziz.kachwala@silverwingteam.com>
2024-03-21 12:32:54 +0530

    changes

---
+++ app/src/main/java/com/mycompany/practice/new1/AddCategoryFragment.java
@@ -0,0 +1,79 @@
+package com.mycompany.practice.new1;
+
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.DialogFragment;
+
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.mycompany.practice.R;
+import com.mycompany.practice.databinding.FragmentAddCategoryBinding;
+
+import java.util.Objects;
+
+public class AddCategoryFragment extends DialogFragment {
+
+    FragmentAddCategoryBinding binding;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(STYLE_NORMAL, R.style.FullScreenDialogStyleTrans);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Dialog dialog = getDialog();
+        if (dialog != null) {
+            Objects.requireNonNull(dialog.getWindow()).setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+            Objects.requireNonNull(dialog.getWindow()).setGravity(Gravity.CENTER);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_add_category, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        try {
+            init();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void init() {
+
+        binding.imgCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        binding.btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(requireContext(), "Added", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
--- app/src/main/java/com/mycompany/practice/new1/AddProductActivity.java
+++ app/src/main/java/com/mycompany/practice/new1/AddProductActivity.java
@@ -32,7 +32,9 @@ public class AddProductActivity extends AppCompatActivity {
         binding.tvAddNewCategory.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(AddProductActivity.this, "New Category", Toast.LENGTH_SHORT).show();
+                AddCategoryFragment addCategoryFragment = new AddCategoryFragment();
+                addCategoryFragment.setCancelable(false);
+                addCategoryFragment.show(getSupportFragmentManager(), "Category");
             }
         });
 
