17dcece93e37cef9c3b9ae6729016bd93ba0a6d4
Aziz Kachwala <aziz.kachwala@silverwingteam.com>
2024-03-22 13:26:35 +0530

    changes

--- app/src/main/java/com/mycompany/practice/new1/AddProductActivity.java
+++ app/src/main/java/com/mycompany/practice/new1/AddProductActivity.java
@@ -1,18 +1,27 @@
 package com.mycompany.practice.new1;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import com.bumptech.glide.Glide;
 import com.mycompany.practice.R;
 import com.mycompany.practice.databinding.ActivityAddProductBinding;
 
 public class AddProductActivity extends AppCompatActivity {
 
     ActivityAddProductBinding binding;
+
+    ActivityResultLauncher<Intent> waitResultForResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +44,20 @@ public class AddProductActivity extends AppCompatActivity {
                 AddCategoryFragment addCategoryFragment = new AddCategoryFragment();
                 addCategoryFragment.setCancelable(false);
                 addCategoryFragment.show(getSupportFragmentManager(), "Category");
+            }
+        });
+        waitResultForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            if (result.getResultCode() == Activity.RESULT_OK) {
+                if (result.getData() != null) {
+
+                }
+            }
+        });
+        binding.imgAddImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                waitResultForResult.launch(camera_intent);
             }
         });
 
